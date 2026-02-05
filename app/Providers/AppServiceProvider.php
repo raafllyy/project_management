38dcdf9f9d4a541ca3e5b0a6dc978902f8ac5c2a
raafllyy <vanilaarfly.com>
2026-02-06 05:27:36 +0700

    fix: force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,22 +3,17 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\URL; // <-- Tambahkan ini
 
 class AppServiceProvider extends ServiceProvider
 {
-    /**
-     * Register any application services.
-     */
-    public function register(): void
-    {
-        //
-    }
+    public function register(): void { }
 
-    /**
-     * Bootstrap any application services.
-     */
     public function boot(): void
     {
-        //
+        // Paksa semua link/form menggunakan HTTPS jika di production
+        if (config('app.env') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
